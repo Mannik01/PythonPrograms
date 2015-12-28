09730acec4ab8c461f51ebe19feda7d9c9f00e46
Mandip Silwal <mannik456@gmail.com>
2015-12-28 16:23:18 -0600

    Added comments

--- Spyder.py
+++ Spyder.py
@@ -1,6 +1,9 @@
-import requests
+# make sure to install the modules if not present
+
+import requests  
 from bs4 import BeautifulSoup
 
+# Function that takes in number of pages to crawl as parameter and collects the titles and links to the respective titles
 def trade_spyder(max_pages):
     page = 1
     while page <= max_pages:
@@ -10,12 +13,12 @@ def trade_spyder(max_pages):
         soup = BeautifulSoup(plain_text, "html.parser")
 
         for link in soup.findAll('a', {'class': 'js-google-analytics__list-event-trigger t-link -color-inherit -decoration-reversed'}):
-            href = "http://codecanyon.net" + link.get('href')
-            title = link.string
+            href = "http://codecanyon.net" + link.get('href')  # gets the link and stores to 'href'
+            title = link.string # gets the title of the items corresponding to the links and stores in 'title'
             print(href)
             print(title)
 
-        page += 1
+        page += 1  # updates page number
 
 trade_spyder(1)
 
