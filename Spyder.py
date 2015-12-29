d04f9a030998abbb9d0bb70e20d98d55699897a5
MandipSilwal <mannik456@gmail.com>
2015-12-28 23:45:04 -0600

    Added a funtion to gather more infor froma single item on the main page

--- Spyder.py
+++ Spyder.py
@@ -14,13 +14,24 @@ def trade_spyder(max_pages):
 
         for link in soup.findAll('a', {'class': 'js-google-analytics__list-event-trigger t-link -color-inherit -decoration-reversed'}):
             href = "http://codecanyon.net" + link.get('href')  # gets the link and stores to 'href'
-            title = link.string # gets the title of the items corresponding to the links and stores in 'title'
-            print(href)
-            print(title)
+            #title = link.string # gets the title of the items corresponding to the links and stores in 'title'
+            #print(href)
+            #print(title)
+            get_item_data(href)
 
         page += 1  # updates page number
 
-trade_spyder(1)
+# Funtion that goes to individual item's page and prints the author's name
+def get_item_data(item_url):
+    source_code = requests.get(item_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, "html.parser")
+
+    for item_name in soup.findAll('a', {'rel': 'author'}):
+        print(item_name.string)
+
+
+trade_spyder(1)  # the argument represents the number of pages to be crawled
 
 
 
