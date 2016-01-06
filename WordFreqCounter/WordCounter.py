1576340c6459744fb1b95cbbc121d5335253555f
MandipSilwal <mannik456@gmail.com>
2016-01-05 19:37:00 -0600

    added a funtion to sort only words

--- WordFreqCounter/WordCounter.py
+++ WordFreqCounter/WordCounter.py
@@ -10,8 +10,19 @@ def begin(url):
         content = post_text.text
         words = content.lower().split()
         for each_word in words:
-            print(each_word)
+
             word_list.append(each_word)
+    clean_a_list(word_list)
 
+# function to sort out only words
+def clean_a_list(word_list):
+    clean_words_list = []
+    for words in word_list:
+        symbols = "!@#$%^&*()_-+={[}]|:;/<,>.?/"
+        for k in range(0, len(symbols)):
+            words = words.replace(symbols[k], "")
+        if len(words) > 0:
+            print(words)
+            clean_words_list.append(words)
 
-begin("https://www.thenewboston.com/forum/category.php?id=10")
+begin("https://www.thenewboston.com/forum/category.php?id=10") # starts the process of word counting from the website in the parenthesis
